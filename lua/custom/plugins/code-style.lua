037c46dbc9be0187ea496c4e2b017d47c5b8051d
d-kja <89911360+d-kja@users.noreply.github.com>
2024-05-05 11:53:30 -0300

    update git w lazy config

--- lua/custom/plugins/code-style.lua
+++ lua/custom/plugins/code-style.lua
@@ -80,19 +80,6 @@ return {
     end,
   },
 
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,32 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- preview hunk
+        map('n', '<leader>Gp', gitsigns.preview_hunk)
+
+        -- Git blame
+        map('n', '<leader>Gb', gitsigns.toggle_current_line_blame)
+
+        -- diff per line?
+        map('n', '<leader>Gd', gitsigns.diffthis)
+      end,
+    }
+  end,
+}
