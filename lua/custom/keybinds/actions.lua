7f4d40450349daff427cb8712f8f254eda27ac1c
d-kja <89911360+d-kja@users.noreply.github.com>
2024-08-31 22:35:47 -0300

    yep

--- lua/custom/keybinds/actions.lua
+++ lua/custom/keybinds/actions.lua
@@ -5,6 +5,8 @@ end)
 
 -- Moves to the left/right
 vim.keymap.set('n', '<leader>ga', '0', { desc = 'Go to the start of the line' })
+vim.keymap.set('n', '<leader>c<Left>', '0', { desc = 'Go to the start of the line' })
 vim.keymap.set('n', '<leader>gd', '$', { desc = 'Go to the end of the line' })
+vim.keymap.set('n', '<leader>c<Right>', '$', { desc = 'Go to the end of the line' })
 
 return {}
